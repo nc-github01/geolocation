a239ca101fbb11082e48968ec23b46560355fab1
First Last <you@email.com>
2017-11-10 13:53:43 +0000

    Geolocation 3nd commit

--- geolocation/force-app/main/default/aura/AccountList/AccountListController.js
+++ geolocation/force-app/main/default/aura/AccountList/AccountListController.js
@@ -1,5 +1,12 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event) {
+        var action = component.get("c.findAll");
+        action.setCallback(this, function(a) {
+            component.set("v.accounts", a.getReturnValue());
+            var event = $A.get("e.c:AccountsLoaded");
+            event.setParams({"accounts": a.getReturnValue()});
+            event.fire();
+        });
+    $A.enqueueAction(action);
     }
 })
